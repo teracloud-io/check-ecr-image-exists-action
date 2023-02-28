280dc32057ef586bde5d4ebe57881a0e41df3f56
jw-tera <juan.wiggenhauser@teracloud.io>
2023-02-28 12:48:39 -0300

    Adding actiono

---
+++ README.md
@@ -0,0 +1,2 @@
+# check-ecr-image-exists-action
+Github Action to check if an ECR image exists with a certain tag
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const AWS = require('aws-sdk');
+
+const listImages = async (params, ecrClient) => {
+  console.log(`Getting repository images...`);
+  const results = await ecrClient.listImages(params).promise();
+  console.log(`Success. Got ${results.deployments.length} deployments`);
+  return results;
+};
+
+const filterResults = (results, imageTag) => {
+  const imageIds = results.imageIds;
+  const tags = imageIds.map(function(image) {
+    return image.imageTag;
+  });
+
+  if (tags.includes(imageTag)) {
+    console.log(`The image tag ${imageTag} exists on the repo`);
+    core.setOutput('exists', 1);
+  } else {
+    console.log(`The image tag ${imageTag} doesn't exists on the repo`);
+    core.setOutput('exists', 0);
+  }
+};
+
+const catchError = (error) => {
+  console.error(error);
+  core.setFailed(error);
+};
+
+try {
+  const regionName = core.getInput('region');
+  const ecrRepo = core.getInput('ecr-repo');
+  const imageTag = core.getInput('image-tag');
+  const ecrClient = new AWS.ECR({region: regionName});
+  const params = {
+    filter: {
+      tagStatus: 'TAGGED',
+    },
+    repositoryName: ecrRepo,
+  };
+  listImages(params, ecrClient)
+      .then((results) => filterResults(results, imageTag))
+      .catch(catchError);
+} catch (error) {
+  core.setFailed(error.message);
+}
